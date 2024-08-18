24e02ab54781be810408dbaa1ca3cdc705323549
Thariq <thariqshah@gmail.com>
2024-08-18 14:56:06 +0530

    first commit

---
+++ src/test/java/demo/aristocat/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.aristocat.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
